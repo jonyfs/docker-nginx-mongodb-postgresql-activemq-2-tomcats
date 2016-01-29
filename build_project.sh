1160c9be11c3f5d2d0e26f4cf8d70edd3750a597
Jony Santos <jonyferreira@hotmail.com>
2016-01-29 18:45:03 -0200

    initial dev

--- build_project.sh
+++ build_project.sh
@@ -12,31 +12,31 @@
 ########################################################################
 
 # clone project
-git clone -b master \
-  --single-branch https://github.com/garystafford/spring-music-docker.git && 
-cd spring-music-docker
+# git clone -b master \
+#   --single-branch https://github.com/garystafford/spring-music-docker.git && 
+# cd spring-music-docker
 
 # build VM
-docker-machine create --driver virtualbox springmusic --debug
+# docker-machine create --driver virtualbox testserver --debug
 
 # create directory to store mongo data on host
-docker-machine ssh springmusic mkdir /opt/mongodb
+# docker-machine ssh testserver mkdir /opt/mongodb
 
 # set new environment
-docker-machine env springmusic && \
-eval "$(docker-machine env springmusic)"
+#docker-machine env testserver && \
+#eval "$(docker-machine env testserver)"
 
 # build images and containers
-docker-compose -f docker-compose.yml -p music up -d
+docker-compose -f docker-compose.yml -p testserver up -d
 
 # configure local DNS resolution for application URL
 #echo "$(docker-machine ip springmusic)   springmusic.com" | sudo tee --append /etc/hosts
 
 # wait for container apps to start
-sleep 15
+#sleep 15
 
 # run quick test of project
-for i in {1..10}
-do
-  curl -I --url $(docker-machine ip springmusic)
-done
+#for i in {1..10}
+#do
+#  curl -I --url $(docker-machine ip testserver)
+#done
